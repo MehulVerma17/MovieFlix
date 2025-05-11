4cb9a9282135ff61b0c7797155df7474185a56b4
Mehul001 <mehulverma17@gmail.com>
2025-05-12 00:47:17 +0530

    first commit

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@": "./", // You can also customize this if you want more specific aliases
+          },
+        },
+      ],
+    ],
+  };
+};
---
+++ components/SearchBar.tsx
@@ -0,0 +1,37 @@
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Dimensions, Image, TextInput, View } from "react-native";
+
+const screenWidth = Dimensions.get("window").width;
+const cardWidth = screenWidth * 0.11; // 30% of screen width
+
+interface Props {
+  placeholder: string;
+  onPress?: () => void;
+}
+
+const SearchBar = ({ placeholder, onPress }: Props) => {
+  return (
+    <View
+      className="flex-row items-center bg-dark-200 rounded-full px-5 py-4 h"
+      style={{ height: cardWidth * 1.5 }}
+    >
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor="#a8b5db"
+        className="flex-1 ml-2 text-white"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
